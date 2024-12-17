e7ca02fef0f920e3ee269d998ddc7caa6d6d8738
KRAUT, Jacob <jacobmatthewkraut@hotmail.com>
2024-12-17 21:50:56 +1000

    Slapped it into a for loop, needs refactoring, want to add html parsing to I can get the puzzle description

--- get_day.go
+++ get_day.go
@@ -8,37 +8,43 @@ import (
 )
 
 func main() {
-	day := "1"
-	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("https://adventofcode.com/2024/day/%s/input", day), nil)
-	req.Header.Set("Cookie", "session=")
 
-	if err != nil {
-		panic(err)
+	numAvailableDays := 17
+	sessionToken := os.Getenv("aoc_session_token")
+
+	for i := 1; i <= numAvailableDays; i++ {
+		req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("https://adventofcode.com/2024/day/%d/input", i), nil)
+		req.Header.Set("Cookie", "session="+sessionToken)
+
+		if err != nil {
+			panic(err)
+		}
+
+		resp, err := http.DefaultClient.Do(req)
+
+		if err != nil {
+			panic(err)
+		}
+
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+
+		if err != nil {
+			panic(err)
+		}
+
+		err = os.MkdirAll(fmt.Sprintf("day_%d", i), 0755)
+		os.Chdir(fmt.Sprintf("day_%d", i))
+
+		if err != nil {
+			panic(err)
+		}
+
+		file, err := os.Create("input.txt")
+		defer file.Close()
+
+		file.Write(body)
+		file.Sync()
+		os.Chdir("..")
 	}
-
-	resp, err := http.DefaultClient.Do(req)
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-
-	if err != nil {
-		panic(err)
-	}
-
-	err = os.MkdirAll(fmt.Sprintf("day_%s", day), 0755)
-	os.Chdir(fmt.Sprintf("day_%s", day))
-
-	if err != nil {
-		panic(err)
-	}
-
-	file, err := os.Create("input.txt")
-	defer file.Close()
-
-	file.Write(body)
-	file.Sync()
 }
